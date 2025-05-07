3a03e57176646c2c2d14060d41cc5fac931ecd03
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2025-05-07 15:25:26 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,35 +1,17 @@
 let distanceText = document.getElementById("distance");
 let mainImg = document.getElementById("mainimg");
 let count = 0;
-let isflying = false;
-let flycount = 0;
 
 
 count = localStorage.getItem("count");
 
 distanceText.textContent = "Distance: " + localStorage.getItem("count");
 
-if(isflying == true) {
-  mainImg.setAttribute("src", "/images/rocket1.png");
-  flycount++;
-}
 
-if(flycount >= 10)
-{
-  isflying = false;
-  flycount = 0;
-}
-
-if(isflying == false) {
-  mainImg.setAttribute("src", "/images/rocket1_off.png");
-}
-
-console.log(isflying + " " + flycount);
 
 function fly() {
-  isflying = true;
 
-  
+  setInterval(isflying, 6000);
 
   count++;
   
@@ -38,5 +20,11 @@ function fly() {
   distanceText.textContent = "Distance: " + localStorage.getItem("count");
 
   
+  
+}
+
+function isflying() {
+
+  mainImg.setAttribute("src", "/images/rocket1.png");
   
 }
