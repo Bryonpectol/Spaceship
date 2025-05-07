615cf652a146e76414ee9f2b57bed2186691be31
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2025-05-07 13:19:51 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+let distanceText = document.getElementById("distance");
+let count;
 
+count = localStorage.getItem("count");
+function fly() {
+
+  count += 1;
+  
+  localStorage.setItem("count", count);
+
+  distanceText.text = "Distance: " + localStorage.getItem("count");
+  
+}
