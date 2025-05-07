5e846bf36b66294a39e1706fe5f24ba5203ced38
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2025-05-07 14:49:11 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 let distanceText = document.getElementById("distance");
+let mainImg = document.getElementById("mainimg");
 let count = 0;
+
 
 count = localStorage.getItem("count");
 
 distanceText.textContent = "Distance: " + localStorage.getItem("count");
 function fly() {
 
+  mainImg.setAttribute("src", "/images/rocket1.png");
+
   count++;
   
   localStorage.setItem("count", count);
 
   distanceText.textContent = "Distance: " + localStorage.getItem("count");
+
+  mainImg.setAttribute("src", "/images/rocket1_off.png");
   
 }
